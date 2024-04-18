d3c28ff28b256d0ea57eacca5f5a9214979f57cc
Kamlesh Bisht <kamleshbisht04@gmail.com>
2024-04-18 12:36:02 +0530

    Modify UI and made working login page

--- loginFrom/login.js
+++ loginFrom/login.js
@@ -1,23 +1,24 @@
-
 'use strict';
-
 
 let title = document.querySelector('.title');
 let nameFeild = document.querySelector('.nameFeild');
 let nameFeildIcan = document.querySelector('.nameFeildIcan');
 let logInBtn = document.getElementById('logInBtn');
 let signUpBtn = document.getElementById('signUpBtn');
-let sign = document.querySelector('.sign');
-let signUp = document.querySelector('.signUp');
+let signUpLine = document.querySelector('.signUpLine');
+let disable = document.querySelector('.disable');
 
 
-logInBtn.addEventListener('click',()=>{
- title.innerHTML ='LogIn';
- nameFeild.classList.add('nameFeild');
- nameFeildIcan.classList.add('nameFeildIcan');
- sign.classList.add('sign');
- logInBtn.classList.remove('sign');
 
+logInBtn.addEventListener('click', () => {
+  title.innerHTML = 'LogIn';
+  nameFeild.classList.add('nameFeild');
+  nameFeildIcan.classList.add('nameFeildIcan');
+  signUpBtn.classList.add('disable');
+  logInBtn.classList.remove('disable');
+  signUpLine.innerHTML = 'Not a Member ?'; 
+  
+  
 });
 
 
@@ -25,9 +26,10 @@ logInBtn.addEventListener('click',()=>{
 signUpBtn.addEventListener('click', () => {
   title.innerHTML = 'Sign Up';
   nameFeild.classList.remove('nameFeild');
- nameFeildIcan.classList.remove('nameFeildIcan');
- sign.classList.remove('sign');
- logInBtn.classList.add('sign');
- signUp.innerHTML=" ";
-
+  nameFeildIcan.classList.remove('nameFeildIcan');
+  signUpBtn.classList.remove('disable');
+  logInBtn.classList.add('disable');
+  signUpLine.innerHTML= "Password Sugesstion"; 
+ 
+  
 });
