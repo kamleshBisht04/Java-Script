a0cd9c4db27c21fc22934da03b9cae4b2ddfc232
Kamlesh Bisht <kamleshbisht04@gmail.com>
2024-04-20 10:24:33 +0530

    complete login page and add funcanality

--- signUpFrom/login.js
+++ signUpFrom/login.js
@@ -1,40 +1,28 @@
 "use strict";
 
 let title = document.querySelector('.title');
-let nameFeild = document.querySelector('.nameFeild');
-let nameFeildIcan = document.querySelector('.nameFeildIcan');
-let LogInBtn = document.querySelector('.LogInBtn');
-let signUpBtn = document.querySelector('.signUpBtn');
-let disable = document.querySelector('.disable');
-let btnclr = document.querySelector('.btn-clr');
-let text = document.querySelector('text');
 let underline = document.querySelector('.underline');
+let nameFeild = document.querySelector('.nameFeild');
+let text = document.querySelector('.text');
+let butnsignUp = document.getElementById('buttonsignUp');
+let butnsignIn = document.getElementById('buttonsignIn');
 
 
-signUpBtn.addEventListener('click', () => {
-  title.innerHTML = 'Sign Up';
-  underline.style.transform = 'translate(18px)';
-  nameFeild.classList.remove('nameFeild')
-  nameFeildIcan.classList.remove('nameFeildIcan')
-  signUpBtn.classList.remove('disable')
-  LogInBtn.classList.add('disable')
-  
-  
-
-});
-
-
-LogInBtn.addEventListener('click',()=>{
-  title.innerHTML='Login';
- underline.style.transform = 'translate(0)';
-nameFeild.classList.add('nameFeild');
-nameFeildIcan.classList.add('nameFeildIcan');
-signUpBtn.classList.add('disable');
-LogInBtn.classList.remove('disable');
-
+butnsignIn.addEventListener('click', () => {
+  title.innerHTML = 'LogIn';
+  nameFeild.style.maxHeight = '0';
+  underline.style.transform ='translateX(25px)';
+  butnsignIn.classList.remove('disable')
+  butnsignUp.classList.add('disable');
+  text.innerHTML="Lost Password ?"
 
 });
 
-
-
-
+butnsignUp.addEventListener('click', () => {
+  title.innerHTML = 'SignUp';
+  nameFeild.style.maxHeight = '50px';
+  underline.style.transform = 'translateX(10px)';
+   butnsignIn.classList.add('disable');
+   butnsignUp.classList.remove('disable');
+   text.innerHTML = 'Password Suggestions ?';
+});
