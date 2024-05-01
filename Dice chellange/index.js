d93a8784d4714ae41e2653f38d16350179bfe9c0
Kamlesh Bisht <kamleshbisht04@gmail.com>
2024-05-01 19:36:50 +0530

    change the UI and Random Dice display via java script on screen

--- Dice chellange/index.js
+++ Dice chellange/index.js
@@ -1,32 +1,27 @@
 'use strict';
 
+const dice1 = document.querySelector('.dice1')
+const dice2 = document.querySelector('.dice2')
+const title=document.querySelector('.title');
 
-const play = document.getElementById('play');
+const playBtn = document.getElementById('play');
+
+let str;
 
 
-play.addEventListener('click', function () {
-  let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+const playGame = function(){
+   str= '';
+  let dice1Random =Math.round(Math.random()*5+1);
+  let dice2Random =Math.round(Math.random()*5+1);
+   
+  // setting the image over dice 
 
-  let randomDiceImage = 'dice' + randomNumber1 + '.png'; //dice1.png - dice6.png
+   dice1.src =`images/dice${dice1Random}.png`;
+   dice2.src =`images/dice${dice2Random}.png`;
 
-  let randomImageSource = 'images/' + randomDiceImage; //images/dice1.png - images/dice6.png
+   
 
-  let image1 = document.querySelectorAll('img')[0];
 
-  image1.setAttribute('src', randomImageSource);
+}
 
-  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-  let randomImageSource2 = 'images/dice' + randomNumber2 + '.png';
-
-  document.querySelectorAll('img')[1].setAttribute('src', randomImageSource2);
-
-  //If player 1 wins
-  if (randomNumber1 > randomNumber2) {
-    document.querySelector('h1').innerHTML = '🚩 Play 1 Wins!';
-  } else if (randomNumber2 > randomNumber1) {
-    document.querySelector('h1').innerHTML = 'Player 2 Wins! 🚩';
-  } else {
-    document.querySelector('h1').innerHTML = 'Draw!';
-  }
-});
+playBtn.addEventListener('click',playGame);
