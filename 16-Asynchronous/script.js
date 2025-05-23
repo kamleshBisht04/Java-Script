d768e248c531e9e4feaae36c800bac9db18b7394
Kamlesh Bisht <kamleshbisht04@gmail.com>
2025-05-23 12:28:58 +0530

    Call back Hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -11,24 +11,60 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+// first AJAX call reandering the country data
 
-const getCountryData= function(country){
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
-    request.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
-        // html handling
-        const flag =data.flags.png;
-        const region=data.region;
-        const population=+(data.population/1000000).toFixed(1);
-        const countryName=data.name.common;
-        const languages = Object.values(data.languages).at(0);
-        const currencies = Object.values(data.currencies).at(0).name;
+// const getCountryData= function(country){
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//     request.send();
+//     request.addEventListener('load', function () {
+//         const [data] = JSON.parse(this.responseText);
+//         console.log(data);
+//         // html handling
+//         const flag =data.flags.png;
+//         const region=data.region;
+//         const population=+(data.population/1000000).toFixed(1);
+//         const countryName=data.name.common;
+//         const languages = Object.values(data.languages).at(0);
+//         const currencies = Object.values(data.currencies).at(0).name;
 
-        const html = `
-        <article class="country">
+//         const html = `
+//         <article class="country">
+//         <img class="country__img" src="${flag}" />
+//         <div class="country__data">
+//         <h3 class="country__name">${countryName}</h3>
+//         <h4 class="country__region">${region}</h4>
+//         <p class="country__row"><span>👫</span>${population} million people</p>
+//         <p class="country__row"><span>🗣️</span>${languages}</p>
+//         <p class="country__row"><span>💰</span>${currencies}</p>
+//         </div>
+//         </article>
+//         `;
+//         countriesContainer.insertAdjacentHTML("beforeend",html);
+//         countriesContainer.style.opacity =1;
+
+//     });
+// }
+// getCountryData('india');
+// getCountryData('usa');
+// getCountryData('uk');
+// getCountryData('chine');
+
+/////////////////////////////////////////
+
+// callBack hell
+
+const rendringCountry = function (data, className = '') {
+  // html handling
+  const flag = data.flags.png;
+  const region = data.region;
+  const population = +(data.population / 1000000).toFixed(1);
+  const countryName = data.name.common;
+  const languages = Object.values(data.languages).at(0);
+  const currencies = Object.values(data.currencies).at(0).name;
+
+  const html = `
+        <article class="country ${className}">
         <img class="country__img" src="${flag}" />
         <div class="country__data">
         <h3 class="country__name">${countryName}</h3>
@@ -39,15 +75,50 @@ const getCountryData= function(country){
         </div>
         </article>
         `;
-        countriesContainer.insertAdjacentHTML("beforeend",html);
-        countriesContainer.style.opacity =1;
-        
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// callBack hell start
+
+const getCountryAndNeighbour = function (country) {
+  //AJAx call 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // rendring country data 1
+    rendringCountry(data);
+
+    // get neighbour country Ajax call 2 callBack hell
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      rendringCountry(data2, 'neighbour');
     });
-}
-getCountryData('india');
-getCountryData('usa');
-getCountryData('uk');
-getCountryData('chine');
+  });
+};
 
+getCountryAndNeighbour('india');
+// getCountryAndNeighbour('usa');
 
+// another example of callBack hell
 
+setTimeout(
+  () => console.log('1 second passed'),
+  setTimeout(() => console.log('2 second passed'), 
+   setTimeout(() => console.log('3 second passed'),
+    setTimeout(() => console.log('4 second passed'), 
+  4000), 
+  3000),
+  2000),
+  1000
+);
