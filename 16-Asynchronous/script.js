e382c81b1232c95b57a7f12c8299491868e653d2
Kamlesh Bisht <kamleshbisht04@gmail.com>
2025-05-27 08:46:54 +0530

    async function

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -502,6 +502,8 @@ wait(1)
 
 // Promissfy
 
+/*
+
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     // navigator.geolocation.getCurrentPosition(
@@ -545,3 +547,120 @@ const whereAmI = function () {
     .catch(err => console.error(`${err.message}💥`));
 };
 btn.addEventListener('click', whereAmI);
+
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+
+GOOD LUCK 😀
+*/
+
+// const imgContainer = document.querySelector('.images');
+
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// const createImage = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     const img = document.createElement('img');
+//     img.src = imgPath;
+
+//     img.addEventListener('load', function () {
+//       imgContainer.append(img);
+//       resolve(img);
+//     });
+
+//     img.addEventListener('error', function () {
+//       reject(new Error('Images not founds'));
+//     });
+//   });
+// };
+
+// let currentImg;
+
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('image 1 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   }).then(img=>{
+//     currentImg= img;
+//     console.log('image 2 loaded');
+//     return wait (2);
+//   }).then(()=>{
+//     currentImg.style.display ='none';
+//   })
+//   .catch(err => console.log(err));
+
+/////////////////////////////////////////////////
+// consuming promises with async and await
+
+// const whereAmI = async function (country) {
+//   const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+
+//   const data = await res.json();
+//   console.log(data[0]);
+//   rendringCountry(data[0]);
+// };
+
+// btn.addEventListener('click', function () {
+//   whereAmI('india');
+// });
+
+/////////////////////////////////////////////////
+// complete with geolocation
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmI = async function () {
+  //geolocation
+  const pos = await getPosition();
+  const { latitude: lat, longitude: lng } = pos.coords;
+  // reverse geocoding
+  const resGeo = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`);
+  const dataGeo = await resGeo.json();
+  // console.log(dataGeo);
+
+  // country data
+  const res = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.countryName}`);
+
+  const data = await res.json();
+  // console.log(data);
+  rendringCountry(data[0]);
+};
+
+btn.addEventListener('click', function () {
+  whereAmI();
+});
